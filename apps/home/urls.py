2c8ea91853f184cd4d8408d86d9e0cf324ae5adc
Deesooja <deesooja24dp@gmial.com>
2023-02-07 20:20:01 +0530

    changes profile.html apply if conditions

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -9,21 +9,12 @@ from apps.home.views import *
 
 urlpatterns = [
 
-    # The home page
     path('', IndexView.as_view(), name='home'),
-    # path('/profile/', views.profile, name='profile'),
-    
-
-    # Matches any html file
-    # re_path(r'^.*\.*', views.pages, name='pages'),
-    # path('',)
-    # path('ajax/', AjaxExampleView.as_view(), name='my_ajax_view'),
     path('icons/', IconsView.as_view(), name='icons'),
     path('google/', GoogleView.as_view(), name='google'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('tables/', TableView.as_view(), name='tables'),
     path('product-page/', ProductView.as_view(), name='product_page'),
     path('user-logout/', LogoutView.as_view(), name='logout'),
-
 
 ]
